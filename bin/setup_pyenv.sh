c7bdd78d53f0182c3f89f52b8795e5da88ff4ac1
Aaron <aaronsgithub@users.noreply.github.com>
2017-11-26 23:53:35 +0000

    Change default python to 3.6.3 in setup_pyenv.sh

--- bin/setup_pyenv.sh
+++ bin/setup_pyenv.sh
@@ -28,7 +28,7 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev
 
-# install python 3.6.1 as default
-pyenv install 3.6.1
-pyenv global 3.6.1
+# install python 3.6.3 as default
+pyenv install 3.6.3
+pyenv global 3.6.3
 
